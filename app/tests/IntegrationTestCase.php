b0b8f328c2c042df2345ec1d8022c3c187fe270e
sabri taieb <sabritaiebdev@gmail.com>
2024-06-21 16:17:34 +0100

    Update IntegrationTestCase by adding entityManager property

--- app/tests/IntegrationTestCase.php
+++ app/tests/IntegrationTestCase.php
@@ -3,21 +3,31 @@
 namespace App\Tests;
 
 use App\Tests\Trait\DatabaseResetter;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class IntegrationTestCase extends KernelTestCase
 {
     use DatabaseResetter;
 
+    protected ?EntityManagerInterface $entityManager;
+
     protected function setUp(): void
     {
         parent::setUp();
         static::bootKernel();
+        self::resetDb();
+
+        $this->entityManager = static::getContainer()->get('doctrine')->getManager();
     }
 
     protected function tearDown(): void
     {
         self::resetDb();
+
+        // doing this is recommended to avoid memory leaks
+        $this->entityManager->close();
+        $this->entityManager = null;
 
         parent::tearDown();
     }
