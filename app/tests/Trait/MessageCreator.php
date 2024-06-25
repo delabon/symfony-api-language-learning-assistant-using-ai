13b46f7b8637db663827cae76604cf5d5c1e4c2a
sabri taieb <sabritaiebdev@gmail.com>
2024-06-25 17:08:09 +0100

    Add MessageCreator trait

---
+++ app/tests/Trait/MessageCreator.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Tests\Trait;
+
+use App\Doctrine\MessageAuthorEnum;
+use App\Entity\Conversation;
+use App\Entity\Message;
+use DateTimeImmutable;
+use ReflectionClass;
+
+trait MessageCreator
+{
+    public function createMessage(string $body, MessageAuthorEnum $messageAuthorEnum = MessageAuthorEnum::USER, ?Conversation $conversation= null): Message
+    {
+        $conversation = $conversation ?: new Conversation();
+
+        $message = new Message();
+        $message->setConversation($conversation);
+        $message->setBody($body);
+        $message->setAuthor($messageAuthorEnum);
+        $message->setCreatedAt(new DateTimeImmutable());
+        $message->setUpdatedAt(new DateTimeImmutable());
+
+        return $message;
+    }
+
+    public function createMessageWithId(int $id, string $body, MessageAuthorEnum $messageAuthorEnum = MessageAuthorEnum::USER, ?Conversation $message= null): Message
+    {
+        $message = $this->createMessage($body, $messageAuthorEnum, $message);
+
+        $reflectionClass = new ReflectionClass($message);
+        $prop = $reflectionClass->getProperty('id');
+        $prop->setValue($message, $id);
+
+        return $message;
+    }
+}
